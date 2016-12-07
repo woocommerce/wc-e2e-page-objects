b65e4c0816a2fdd9e434a8d3db6f3dd54402ab35
Akeda Bagus <admin@gedex.web.id>
2016-12-08 03:39:59 +0700

    Fixed lint error.

--- src/components/component-cart-totals.js
+++ src/components/component-cart-totals.js
@@ -2,7 +2,6 @@
  * External dependencies
  */
 import { By } from 'selenium-webdriver';
-import { WebDriverHelper as helper } from 'wp-e2e-webdriver';
 import { Component } from 'wp-e2e-page-objects';
 
 /**
@@ -14,21 +13,5 @@ const SELECTOR = By.css( '.cart_totals' );
 export default class ComponentCartTotals extends Component {
 	constructor( driver, selector = SELECTOR, args = { wait: false, waitMs: 10000 } ) {
 		super( driver, selector, args );
-	}
-
-	hasSubtotal( amountText ) {
-
-	}
-
-	hasShipping( shippingText ) {
-
-	}
-
-	hasTax( taxText ) {
-
-	}
-
-	hasTotal( totalText ) {
-
 	}
 }
