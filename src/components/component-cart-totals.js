420cdc151ce617cddaec2bf459e918ac6133383e
Akeda Bagus <admin@gedex.web.id>
2016-12-13 22:27:47 +0700

    Added subtotal method in ComponentCartTotals.

--- src/components/component-cart-totals.js
+++ src/components/component-cart-totals.js
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import { By } from 'selenium-webdriver';
+import { WebDriverHelper as helper } from 'wp-e2e-webdriver';
 import { Component } from 'wp-e2e-page-objects';
 
 /**
@@ -13,5 +14,13 @@ const SELECTOR = By.css( '.cart_totals' );
 export default class ComponentCartTotals extends Component {
 	constructor( driver, selector = SELECTOR, args = { wait: false, waitMs: 10000 } ) {
 		super( driver, selector, args );
+	}
+
+	hasSubtotal( subtotal ) {
+		const selector = By.xpath( `//tr[@class="cart-subtotal" and .//span[contains(text(), "${ subtotal }")]]` );
+		return helper.isEventuallyPresentAndDisplayed(
+			this.driver,
+			selector
+		);
 	}
 }
