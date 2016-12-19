b15cf2c48f7fe72791016635bc5ebec1dca1cec9
Akeda Bagus <admin@gedex.web.id>
2016-12-19 20:04:36 +0700

    Update exported things in main file.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,50 @@
 /**
  * Internal dependencies
  */
+import * as PageMap from './page-map';
 import * as Helper from './helper';
+import ComponentCheckout from './components/component-checkout';
+import ComponentCheckoutBillingDetails from './components/component-checkout-billing-details';
+import ComponentCheckoutOrderReview from './components/component-checkout-order-review';
+import ComponentCheckoutShippingDetails from './components/component-checkout-shipping-details';
+import ComponentOrderDetails from './components/component-order-details';
+import ComponentOrderCustomerDetails from './components/component-order-customer-details';
+import ComponentOrderBillingAddress from './components/component-order-billing-address';
+import ComponentOrderShippingAddress from './components/component-order-shipping-address';
 import CartPage from './pages/cart-page';
+import CheckoutPage from './pages/checkout-page';
+import CheckoutOrderReceivedPage from './pages/checkout-order-received-page';
 import ShopPage from './pages/shop-page';
 import WPAdminProductNew from './pages/wp-admin/wp-admin-product-new';
 import WPAdminWCSettings from './pages/wp-admin/wp-admin-wc-settings';
 import WPAdminWCSettingsGeneral from './pages/wp-admin/wp-admin-wc-settings-general';
 import WPAdminWCSettingsProductsGeneral from './pages/wp-admin/wp-admin-wc-settings-products-general';
 import WPAdminWCSettingsProductsDownloadable from './pages/wp-admin/wp-admin-wc-settings-products-downloadable';
+import StoreOwnerFlow from './flows/store-owner-flow';
+import GuestCustomerFlow from './flows/guest-customer-flow';
+import CustomerFlow from './flows/customer-flow';
 
 export {
+	PageMap,
 	Helper,
+	ComponentCheckout,
+	ComponentCheckoutBillingDetails,
+	ComponentCheckoutOrderReview,
+	ComponentCheckoutShippingDetails,
+	ComponentOrderDetails,
+	ComponentOrderCustomerDetails,
+	ComponentOrderBillingAddress,
+	ComponentOrderShippingAddress,
 	CartPage,
+	CheckoutPage,
+	CheckoutOrderReceivedPage,
 	ShopPage,
 	WPAdminProductNew,
 	WPAdminWCSettings,
 	WPAdminWCSettingsGeneral,
 	WPAdminWCSettingsProductsDownloadable,
-	WPAdminWCSettingsProductsGeneral
+	WPAdminWCSettingsProductsGeneral,
+	StoreOwnerFlow,
+	GuestCustomerFlow,
+	CustomerFlow
 };
