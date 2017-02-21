d1788e666e1c46851eb2a96414ac09b9fd20eae0
Akeda Bagus <admin@gedex.web.id>
2017-02-21 13:27:47 +0700

    Added setGeneralSettings in StoreOwnerFlow.
    Also make sure settings are saved when calling enabledPaymentMethod.

--- src/flows/store-owner-flow.js
+++ src/flows/store-owner-flow.js
@@ -33,22 +33,57 @@ export default class StoreOwnerFlow extends UserFlow {
 		return this.open( PAGE.WP_ADMIN_WC_SETTINGS_GENERAL );
 	}
 
+	setGeneralSettings( args ) {
+		const settings = this.openGeneralSettings();
+
+		args = Object.assign(
+			{
+				baseLocation: [ 'United States', 'United States (US) — California' ],
+				sellingLocation: 'Sell to all countries',
+				enableTaxes: true,
+				currency: [ 'United States', 'United States dollar ($)' ],
+			},
+			args
+		);
+
+		if ( Array.isArray( args.baseLocation ) && 2 === args.baseLocation.length ) {
+			settings.selectBaseLocation( ...args.baseLocation );
+		}
+
+		if ( args.sellingLocation ) {
+			settings.selectSellingLocation( args.sellingLocation );
+		}
+
+		if ( args.enableTaxes ) {
+			settings.checkEnableTaxes();
+		}
+
+		if ( Array.isArray( args.currency ) && 2 === args.currency.length ) {
+			settings.selectCurrency( ...args.currency );
+		}
+
+		return settings.saveChanges();
+	}
+
 	openCheckoutSettings() {
 		return this.open( PAGE.WP_ADMIN_WC_SETTINGS_CHECKOUT );
 	}
 
 	enableCOD() {
 		this.open( PAGE.WP_ADMIN_WC_SETTINGS_CHECKOUT_COD );
-		return this.currentPage.checkEnable();
+		this.currentPage.checkEnable();
+		return this.currentPage.saveChanges();
 	}
 
 	enableBACS() {
 		this.open( PAGE.WP_ADMIN_WC_SETTINGS_CHECKOUT_BACS );
-		return this.currentPage.checkEnable();
+		this.currentPage.checkEnable();
+		return this.currentPage.saveChanges();
 	}
 
 	enablePayPal() {
 		this.open( PAGE.WP_ADMIN_WC_SETTINGS_CHECKOUT_PAYPAL );
-		return this.currentPage.checkEnable();
+		this.currentPage.checkEnable();
+		return this.currentPage.saveChanges();
 	}
 }
