bfce6d8f065a63b2a669cc666b597c9c01f1dbf5
Claudiu Lodromanean <claudiu@Claudius-MacBook-Pro.local>
2017-02-27 14:10:09 -0800

    Wait for animations to finish before starting toggle

--- src/helper.js
+++ src/helper.js
@@ -5,6 +5,7 @@ import { By } from 'selenium-webdriver';
 import { WebDriverHelper as helper } from 'wp-e2e-webdriver';
 
 const UI_BLOCK_SELECTOR = By.css( '.blockUI.blockOverlay' );
+const ANIMATION_SELECTOR = By.css( ':animated' );
 
 export function waitTillUIBlockPresent( driver, waitMs = 10000 ) {
 	return helper.waitTillPresentAndDisplayed( driver, UI_BLOCK_SELECTOR, waitMs );
@@ -12,6 +13,10 @@ export function waitTillUIBlockPresent( driver, waitMs = 10000 ) {
 
 export function waitTillUIBlockNotPresent( driver, waitMs = 10000 ) {
 	return helper.waitTillNotPresent( driver, UI_BLOCK_SELECTOR, waitMs );
+}
+
+export function waitTillAnimationFinished( driver, waitMs = 10000 ) {
+	return helper.waitTillNotPresent( driver, ANIMATION_SELECTOR, waitMs );
 }
 
 export function waitTillAlertAccepted( driver, waitMs = 10000 ) {
